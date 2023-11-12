34adb024d093d0005c9ec7ddd56c99dc96bd6d51
Gabriel Poersch <gabpoersch@gmail.com>
2023-11-12 14:45:08 -0300

    jwt endpoints

---
+++ server/user_auth/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data.update({'username': self.user.username})
+        return data
